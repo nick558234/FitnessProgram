c02320e251266e20401f3f2eb433ef103f0d3253
nickvanhooff <nickvanhooff03@gmail.com>
2024-11-05 00:29:45 +0100

    Add project files.

---
+++ FitnessProgram/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using FitnessProgram.Models;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FitnessProgram
+{
+    public partial class MainWindow : Window
+    {
+        private List<Exercise> exercises = new List<Exercise>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddExercise_Click(object sender, RoutedEventArgs e)
+        {
+            // Get user input
+            string name = txtName.Text;
+            int duration = int.Parse(txtDuration.Text);
+
+            IntensityLevel intensity = (IntensityLevel)cbIntensity.SelectedIndex;
+
+            // Create new exercise and add to the list
+            Exercise newExercise = new Exercise(name, duration, intensity);
+            exercises.Add(newExercise);
+
+            // Display exercises in the ListBox
+            lstExercises.Items.Add(newExercise);
+
+            // Update total duration
+            UpdateTotalDuration();
+        }
+
+        private void UpdateTotalDuration()
+        {
+            // sum of all exercise durations
+            int totalDuration = exercises.Sum(exercise => exercise.Duration);
+
+            txtTotalDuration.Text = $"Total Duration: {totalDuration} minutes";
+        }
+    }
+
+}
---
+++ FitnessProgram/Models/Exercise.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgram.Models
+{
+    public class Exercise
+    {
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public IntensityLevel Intensity { get; set; } // use enum 'IntensityLevel'
+
+        public Exercise(string name, int duration, IntensityLevel intensity)
+        {
+            Name = name;
+            Duration = duration;
+            Intensity = intensity;
+        }
+
+        public override string ToString()
+        {
+            // Retourneert de oefening in leesbare vorm, bijvoorbeeld: "Push-up - 10 minutes (Medium)"
+            return $"{Name} - {Duration} minutes ({Intensity})";
+        }
+    }
+    public enum IntensityLevel
+    {
+        Low,
+        Medium,
+        High
+    }
+
+}
